3576f2f2f9a1a1b661d239a10d835e2b9862db68
Jose Marti <josemarti@my.tridenttech.edu>
2024-07-23 20:26:17 -0400

    Update lab7-2.cpp

--- lab7-2.cpp
+++ lab7-2.cpp
@@ -10,5 +10,21 @@ using namespace std;
 
 int main()
 {
+
+    int day = 0'
+    int totalTexts = 0;
+    int dailyTexts = 0.0;
+    for (day = 1; day < 8; day += 1)
+{
+    cout << "how many test messages did you send on day "
+        << day << "?";
+    cin >> dailyTexts;
+    totalTexts += dailyTexts;
+}    //end for
+
+average = static_cast<double>(totalTexts) / (day - 1);
+    cout << fixed << setpricsion(0);
+    cout << endl << "You sent approximately "
+        << average << " text messages per day. " << endl;
     return 0;
 } //end of main function
