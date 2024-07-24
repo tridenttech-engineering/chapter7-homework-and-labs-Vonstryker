e7d0d8922df60399564d1814579b4171ba883b34
Jose Marti <josemarti@my.tridenttech.edu>
2024-07-23 21:10:18 -0400

    Update hw7-16.cpp

--- hw7-16.cpp
+++ hw7-16.cpp
@@ -8,19 +8,19 @@ using namespace std;
 int main()
 {
 	double bill = 0.0;
-	double tip = 0.0;
+  double tip = 0.0;
+  double rate = 0.1;
 
-	cout << "Bill amount: ";
-	cin >> bill;
+  cout << "Bill amount: ";
+  cin >> bill;
 
-	cout << fixed;
-	for (double rate = 0.1; rate <= 0.2; rate += 0.05)
-	{
-		tip = bill * rate;
-		cout << setprecision(0);
-		cout << rate * 100 << "% tip: ";
-		cout << setprecision(2);
-		cout << "$" << tip << endl;
-	}   // end for
+  cout << fixed;
+  while (rate <= 0.2) {
+    tip = bill * rate;
+    cout << setprecision(0);
+    cout << rate * 100 << "% tip: ";
+    cout << setprecision(2);
+    cout << "$" << tip << endl;
+    rate += 0.05;
 	return 0;
 }	//end of main function
