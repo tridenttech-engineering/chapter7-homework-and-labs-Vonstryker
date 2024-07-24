221d7d3dbfdb7326afbf5f1130e37fe15521c483
Jose Marti <josemarti@my.tridenttech.edu>
2024-07-23 20:59:16 -0400

    Update lab7-4.cpp

--- lab7-4.cpp
+++ lab7-4.cpp
@@ -7,6 +7,24 @@ using namespace std;
 
 int main()
 {
-
+double bill = 0.0;
+  double totalBills = 0.0;
+  double avgBill = 0.0;
+  int months = 0;
+  cout << "Bill for month 1: ";
+  cin >> bill;
+  while (bill >= 0.0) {
+    totalBills += bill;
+    months += 1;
+    cout << "Bill for month " << months + 1 << ": ";
+    cin >> bill;
+  } // end while
+  if (months > 0) {
+    avgBill = totalBills / months;
+    cout << fixed << setprecision(2);
+    cout << "Average electric bill for " << months << " months: $" << avgBill
+         << endl;
+  } else
+    cout << "No bill amount entered." << endl; // end if
 	return 0;
 }	//end of main function
